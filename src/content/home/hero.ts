4fa90fac7dd63a8dc8215b8229ab82dc6c92e295
Abdulrahman <abdulrahmaper.acc@gmail.com>
2026-05-12 13:38:31 +0300

    Fix company profile PDF download

--- src/content/home/hero.ts
+++ src/content/home/hero.ts
@@ -1,7 +1,8 @@
 import { withBasePath } from "@/lib/site-pages";
 import { sharedCtas } from "./cta";
 
-export const HEPA_PROFILE_DOWNLOAD_PATH = "/documents/hepa-company-profile-2026.pdf";
+export const HEPA_PROFILE_DOWNLOAD_PATH =
+  "https://raw.githubusercontent.com/FirstAidKit-hierarchy/HEPA/main/public/documents/hepa-company-profile-2026.pdf";
 export const HEPA_PROFILE_DOWNLOAD_FILENAME = "HEPA-Company-Profile-2026.pdf";
 
 export const heroContent = {
